52ea208aa1d8f8c7d1324377c4ca4b9a387a5e05
Asako Mikami <afmikami@bu.edu>
2019-04-04 00:25:24 -0400

    finished creating map

--- script/create_map.R
+++ script/create_map.R
@@ -1,90 +1,58 @@
 ####----------------------------
-## Create a map of US congressional district
-## and plot polling locations where pizzas
-## were delivered.
+## This script creates a map of US congressional district
+## and plots polling locations where pizzas
+## were delivered by Pizza To The Polls.
 ####----------------------------
 
-x <- c("dplyr", "purrr", "forcats",
-       "sf", "RColorBrewer", "tmap", "raster", "rgdal",
+x <- c("tidyverse", "magrittr", "RColorBrewer", "rio",
+       "sf", "tmap", "raster", "rgdal",
        "tidycensus")
 lapply(x, library, character.only = TRUE, quietly = TRUE)
-color <- brewer.pal(3, "Set1")
-
-
-# STEP 1: load shape files
-
-## build `shpname`
-dir_url <- "https://www2.census.gov/geo/tiger/GENZ2016/shp/"
-ss <- "us" 
-entity <- c("cd115", # congressional district
-            "county") # county
-rr <- "500k" # resolution
-shpname <- paste("cb_2016", ss, entity, rr, sep = "_")
-
-## download zipfile to temporary file
-shpname %>% map(~ {
-        url <- paste0(dir_url, ., ".zip")
-        temp <- tempfile()
-        download.file(url, destfile = temp)
-        dir <- file.path("data", "shp", .)
-        unzip(temp, exdir = dir)
-})
+color <- brewer.pal(3, "Accent")
+data(fips_codes) # from `tidycensus`
 
 ## read .shp file as SpatialPolygonsDataFrame
-us_cd <- readOGR(dsn = file.path("data", "shp", 
-                                 shpname[grep("cd", shpname)]),
-                 layer = shpname[grep("cd", shpname)])
-us_county <- readOGR(dsn = file.path("data", "shp", 
-                                     shpname[grep("county", shpname)]),
-                      layer = shpname[grep("county", shpname)])
+district <- "cb_2016_us_cd115_500k"
+county <- "cb_2016_us_county_500k"
+us_cd <- readOGR(dsn = file.path("data", "shp", district), layer = district)
+us_county <- readOGR(dsn = file.path("data", "shp", county), layer = county)
 
-
-# STEP 2: clean us_cd 
-
-## join `fips_codes` to us_cd@data
-data(fips_codes) # from tidycensus
-fips_codes <- fips_codes %>% 
-        dplyr::select(-county_code, -county) %>% 
-        map_if(is.character, factor) %>% 
-        as.data.frame() 
-us_cd@data <- us_cd@data %>% dplyr::select(-AWATER, -ALAND) %>%
-        mutate(STATEFP = factor(STATEFP, 
-                                levels(fips_codes$state_code)),
-               STATE = )  
+states <- fips_codes %>% dplyr::select(state, state_code, state_name) %>%
+        filter(!duplicated(.))
+senate <- import(file.path("data", "csv", "senate_elections_2018.csv"),
+                 header = TRUE)
+house <- import(file.path("data", "csv", "house_elections_2018.csv"),
+                header = TRUE)
+us_cd@data %<>% left_join(states, by = c("STATEFP" = "state_code")) %>% 
+        mutate(label = paste(state, CD115FP, sep = "-")) %>%
+        left_join(house, by = c("label" = "V1"))
+        
 
 
 
-## add "senate_elections_2018.csv" columns 
-senate2018 <- read.csv(file.path("data", "csv", 
-                        paste("senate_elections_2018.csv"))) %>%
-                mutate(state = factor(X, levels(fips_codes$state))) %>%
-                dplyr::select(-X)
 
 
-# STEP 3: 
-# import "PizzaToThePolls.csv" as dataframe and convert to sf object
-pizzapolls <- file.path("data", "csv", 
-                        "PizzaToThePolls.csv") %>%
-        read.csv(header = TRUE) %>%
+# import pizzapolls.csv
+pizzapolls <- import(file.path("data", "csv", "PizzaToThePolls.csv"), 
+                     format = "csv", header = TRUE) %>%
         st_as_sf(coords = c("lng", "lat"))
 st_crs(pizzapolls) <- st_crs(us_cd)$proj4string
 
 
 # STEP 4:
 # set up bounding box for coterminous US
-usa_main <- matrix(c(-130, 25, -60, 50), nrow = 2, byrow = FALSE)
+usa_main <- matrix(c(-120, 25, -70, 45), nrow = 2, byrow = FALSE)
 
 
 # STEP 5:
 # create interactive map
 tmap_mode("view")
-tm_basemap("OpenStreetMap.DE") +
-tm_shape(us_cd, bbox = usa_main) + tm_polygons()
-
-+
-tm_shape(us_county, bbox = usa_main) +  
-        tm_borders(lty = "dotted", lwd = 0.5) 
-tm_shape(pizzapolls) +
-        tm_dots(col = color[1]) 
-
+tm <- tm_shape(us_cd, name = "US congressional district", unit = "km",
+         bbox = usa_main) + 
+        tm_polygons(border.col = color[2], id = "label", 
+                    popup.vars = c("state_name", 
+                                   "candidate1", "party1", "share1",
+                                   "candidate2", "party2", "share2")) + 
+        tm_shape(pizzapolls) +
+        tm_markers()
 
---
+++ script/download_data.R
@@ -0,0 +1,23 @@
+###-----------------------
+# This script downloads congressional district shapefile from
+# US Census bureau.
+###-----------------------
+
+
+
+
+dir_url <- "https://www2.census.gov/geo/tiger/GENZ2016/shp/"
+ss <- "us" 
+entity <- c("cd115", # congressional district
+            "county") # county
+rr <- "500k" # resolution
+shpname <- paste("cb_2016", ss, entity, rr, sep = "_")
+
+## download and extract zipfiles
+shpname %>% map(~ {
+        url <- paste0(dir_url, ., ".zip")
+        temp <- tempfile()
+        download.file(url, destfile = temp)
+        dir <- file.path("data", "shp", .)
+        unzip(temp, exdir = dir)
+})
